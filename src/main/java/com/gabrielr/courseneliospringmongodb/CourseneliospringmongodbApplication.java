9730566152bdc0dc48d6f4960dc7ad2a31655938
Gabriel Rocha <fernandeswb7@hotmail.com>
2022-03-31 15:06:59 -0300

    Create Project

---
+++ src/main/java/com/gabrielr/courseneliospringmongodb/CourseneliospringmongodbApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielr.courseneliospringmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseneliospringmongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseneliospringmongodbApplication.class, args);
+	}
+
+}
