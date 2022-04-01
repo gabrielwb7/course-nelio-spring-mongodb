4daf8d4657b043b36c06bb2b95ef67a494c901bb
Gabriel Rocha <fernandeswb7@hotmail.com>
2022-04-01 06:53:25 -0300

    Obtendo usuario por id

--- src/main/java/com/gabrielr/courseneliospringmongodb/resources/UserResource.java
+++ src/main/java/com/gabrielr/courseneliospringmongodb/resources/UserResource.java
@@ -6,6 +6,7 @@ import com.gabrielr.courseneliospringmongodb.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +26,13 @@ public class UserResource {
         List<UserDTO> usersDto = users.stream().map( x -> new UserDTO(x)).collect(Collectors.toList());
 
         return ResponseEntity.ok().body(usersDto);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+        User user = service.findById(id);
+        UserDTO userDto = new UserDTO(user);
+
+        return ResponseEntity.ok().body(userDto);
     }
 }
--- src/main/java/com/gabrielr/courseneliospringmongodb/services/UserService.java
+++ src/main/java/com/gabrielr/courseneliospringmongodb/services/UserService.java
@@ -2,6 +2,7 @@ package com.gabrielr.courseneliospringmongodb.services;
 
 import com.gabrielr.courseneliospringmongodb.domain.User;
 import com.gabrielr.courseneliospringmongodb.repositories.UserRepository;
+import com.gabrielr.courseneliospringmongodb.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,5 +16,9 @@ public class UserService {
 
     public List<User> findAll() {
         return repository.findAll();
+    }
+
+    public User findById(String id) {
+        return repository.findById(id).orElseThrow(() -> new ObjectNotFoundException("O usuario com esse id" + id + "não existe"));
     }
 }
