35c0bdb93ae7f84c999501734145b4274c0910a9
Gabriel Rocha <fernandeswb7@hotmail.com>
2022-04-01 06:13:06 -0300

    Criado o TestConfig

---
+++ src/main/java/com/gabrielr/courseneliospringmongodb/config/TestConfig.java
@@ -0,0 +1,28 @@
+package com.gabrielr.courseneliospringmongodb.config;
+
+import com.gabrielr.courseneliospringmongodb.domain.User;
+import com.gabrielr.courseneliospringmongodb.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class TestConfig implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        userRepository.deleteAll();
+
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
+    }
+}
